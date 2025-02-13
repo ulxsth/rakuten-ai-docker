18e78fe82058b7c894601cd24969ecb2de931acd
ulxsth <ulxsth@gmail.com>
2025-02-13 14:12:14 +0900

    feat: Flaskアプリケーションを追加し、テキスト生成エンドポイントを実装

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+app = Flask(__name__)
+
+model_name = "RakutenAI-2.0-mini-instruct"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name)
+
+@app.route('/generate', methods=['POST'])
+def generate():
+    input_text = request.json['text']
+    inputs = tokenizer.encode(input_text, return_tensors='pt')
+    outputs = model.generate(inputs, max_length=50)
+    generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    return jsonify({'generated_text': generated_text})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
